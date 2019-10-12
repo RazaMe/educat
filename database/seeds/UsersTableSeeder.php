bc1f194f9a9f3dd1fcf4db9f94067b88b1f3f526
dev4 <dev4@shayansolutions.com>
2019-10-12 21:28:49 +0500

    users controller and usersTableSeeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if(User::count() == 0)
+            factory(App\User::class, 50)->create();
+    }
+}
