1e09cee0f86f0b157bfb3f902fc1677cde77e302
dev4 <dev4@shayansolutions.com>
2019-10-13 19:55:29 +0500

    Locale function corrected

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['class' => 'login-page', 'page' => _('Login Page'), 'contentClass' => 'login-page'])
+@extends('layouts.app', ['class' => 'login-page', 'page' => __('Login Page'), 'contentClass' => 'login-page'])
 {{--<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div>--}}
 
 @section('styles')
@@ -29,7 +29,7 @@
                         <img src="/educat-logo.png" alt="" class="center">
                     </div>
 {{--                    <img src="/assets/img/card-primary.png" alt="">--}}
-{{--                    <h1 class="card-title">{{ _('Log in') }}</h1>--}}
+{{--                    <h1 class="card-title">{{ __('Log in') }}</h1>--}}
                 </div>
                 <div class="card-body">
 {{--                    <p class="text-dark mb-2">Sign in with <strong>admin@white.com</strong> and the password <strong>secret</strong></p>--}}
@@ -40,7 +40,7 @@
                                 <i class="tim-icons icon-email-85"></i>
                             </div>
                         </div>
-                        <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ _('Email') }}">
+                        <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}">
                         @include('alerts.feedback', ['field' => 'email'])
                     </div>
                     <div class="input-group{{ $errors->has('password') ? ' has-danger' : '' }}">
@@ -49,20 +49,20 @@
                                 <i class="tim-icons icon-lock-circle"></i>
                             </div>
                         </div>
-                        <input type="password" placeholder="{{ _('Password') }}" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}">
+                        <input type="password" placeholder="{{ __('Password') }}" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}">
                         @include('alerts.feedback', ['field' => 'password'])
                     </div>
                 </div>
                 <div class="card-footer">
-                    <button type="submit" href="" class="btn btn-danger btn-lg btn-block mb-3">{{ _('Get Started') }}</button>
+                    <button type="submit" href="" class="btn btn-danger btn-lg btn-block mb-3">{{ __('Get Started') }}</button>
                     <div class="pull-left">
                         <h6>
-                            <a href="{{ route('register') }}" class="link footer-link" style="color: red">{{ _('Create Account') }}</a>
+                            <a href="{{ route('register') }}" class="link footer-link" style="color: red">{{ __('Create Account') }}</a>
                         </h6>
                     </div>
                     <div class="pull-right">
                         <h6>
-                            <a href="{{ route('password.request') }}" class="link footer-link" style="color: red">{{ _('Forgot password?') }}</a>
+                            <a href="{{ route('password.request') }}" class="link footer-link" style="color: red">{{ __('Forgot password?') }}</a>
                         </h6>
                     </div>
                 </div>
